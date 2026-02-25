c6b9c8ffb2ceb98d4737c07477cfbc0c8b927e7c
Girish Kumar <girishkumar.mukkamala@outlook.com>
2026-02-25 23:07:50 +0530

    Update Docker Compose download URL to latest version

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/usr/local/bin/
 source .bashrc
 
 # Download the current stable release of Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # Apply executable permissions
 sudo chmod +x /usr/local/bin/docker-compose
 # Create a symbolic link
